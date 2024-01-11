503f286a8d80b9678c5f54d10b1b6acd98eb9793
Gavin P <gavinjpeeler@gmail.com>
2024-01-10 18:22:26 -0800

    Basic intake code

--- src/main/java/frc/robot/constants/CAN.java
+++ src/main/java/frc/robot/constants/CAN.java
@@ -2,7 +2,9 @@ package frc.robot.constants;
 
 public final class CAN {
   public static final int pigeon = 9;
-
+  
+  public static final int intakeMotor = 1;
+  
   public static final int frontLeftCanCoder = 10;
   public static final int frontRightCanCoder = 11;
   public static final int backLeftCanCoder = 12;
