996e28e6a2f56e17f1727dd985345035917bed7b
Jax Pham <jaxkpham@gmail.com>
2024-02-02 17:30:37 -0800

    added goal posses

--- src/main/java/frc/robot/constants/FLYWHEEL.java
+++ src/main/java/frc/robot/constants/FLYWHEEL.java
@@ -32,4 +32,36 @@ public final class FLYWHEEL {
       return value;
     }
   }
+
+  public static final double NoteVelocity = 0; // add note velocity
+
+
+  public static class SPEAKER {
+    
+    public static final double BlueSpeakerTLY = 6.1478414;
+    public static final double BlueSpeakerTLX = 0.464947;
+
+    public static final double BlueSpeakerTRY = 5.0232564;
+    public static final double BlueSpeakerTRX = 0.464947;
+
+    public static final double BlueSpeakerBLY = 6.1478414;
+    public static final double BlueSpeakerBLX = 0;
+
+    public static final double BlueSpeakerBRY = 5.0232564;
+    public static final double BlueSpeakerBRX = 0;
+
+
+    
+    public static final double RedSpeakerTLY = 6.1478414;
+    public static final double RedSpeakerTLX = 16.0697672;
+
+    public static final double RedSpeakerTRY = 5.0232564;
+    public static final double RedSpeakerTRX = 16.0697672;
+
+    public static final double RedSpeakerBLY = 6.1478414;
+    public static final double RedSpeakerBLX = 16.5410642;
+
+    public static final double RedSpeakerBRY = 5.0232564;
+    public static final double RedSpeakerBRX = 16.5410642;
+  }
 }
