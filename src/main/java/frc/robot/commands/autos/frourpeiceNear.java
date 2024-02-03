6b8ae2abf84b01f209849527b93f30da2c4a85ff
Ronan <rdavidbouza@gmail.com>
2024-02-03 15:32:25 -0800

    Sim 4 piece auto
    testing on field with gridlock

--- src/main/java/frc/robot/commands/autos/frourpeiceNear.java
+++ src/main/java/frc/robot/commands/autos/frourpeiceNear.java
@@ -4,9 +4,14 @@
 
 package frc.robot.commands.autos;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -21,22 +26,37 @@ public class frourpeiceNear extends SequentialCommandGroup {
   /** Creates a new DriveStraightTest. */
   public frourpeiceNear(CommandSwerveDrivetrain swerveDrive, FieldSim fieldSim) {
 
-    PathPlannerPath path = PathPlannerPath.fromPathFile("TestAuto1");
-
-    var m_ppCommand = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path, 1.0, false);
+    PathPlannerPath path1 = PathPlannerPath.fromPathFile("fourpiecept1");
+var m_ppCommand1 = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path1, path1.getGlobalConstraints().getMaxVelocityMps(), false);
+PathPlannerPath path2 = PathPlannerPath.fromPathFile( "fourpiecept2");
+        var m_ppCommand2 = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path2, path2.getGlobalConstraints().getMaxVelocityMps(), false);
+    PathPlannerPath path3 = PathPlannerPath.fromPathFile( "fourpiecept3");
+        var m_ppCommand3 = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path3, path3.getGlobalConstraints().getMaxVelocityMps(), false);
+        PathPlannerPath path4 = PathPlannerPath.fromPathFile( "fourpiecept4");
+        var m_ppCommand4 = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path4, path4.getGlobalConstraints().getMaxVelocityMps(), false);
+        PathPlannerPath path5 = PathPlannerPath.fromPathFile( "fourpiecept5");
+        var m_ppCommand5 = TrajectoryUtils.generatePPHolonomicCommand(swerveDrive, path5, path5.getGlobalConstraints().getMaxVelocityMps(), false);
 
     var point = new SwerveRequest.PointWheelsAt();
     var stopRequest = new SwerveRequest.ApplyChassisSpeeds();
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
+     ArrayList<PathPlannerPath> pathsList = new ArrayList<>();
+pathsList.add(path1);
+pathsList.add(path2);
+pathsList.add(path3);
+pathsList.add(path4);
+pathsList.add(path5);
+    
     addCommands(
-        new PlotAutoPath(fieldSim, "TestAuto1", path),
+        new PlotAutoPath(fieldSim, "AutoExample", pathsList),
         new InstantCommand(
-            () -> swerveDrive.seedFieldRelative(path.getPreviewStartingHolonomicPose())),
+            () -> swerveDrive.seedFieldRelative(path1.getPreviewStartingHolonomicPose())),
         new InstantCommand(
                 () -> swerveDrive.applyRequest(() -> point.withModuleDirection(new Rotation2d())),
                 swerveDrive)
             .alongWith(new WaitCommand(1)),
-        m_ppCommand.andThen(() -> swerveDrive.setControl(stopRequest)));
+        m_ppCommand1,m_ppCommand2,m_ppCommand3,m_ppCommand4,
+        m_ppCommand5.andThen(() -> swerveDrive.setControl(stopRequest)));
   }
 }
