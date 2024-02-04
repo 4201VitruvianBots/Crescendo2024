a3e82e85025eea1724029754e272fa1d058e5a58
Benjamin Chan <benjamin.v.chan@gmail.com>
2024-02-03 18:43:10 -0800

    Shows 2D Vision Pose in 2d fieldsim

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.VISION;
+import frc.robot.simulation.FieldSim;
 import java.util.List;
 import java.util.Optional;
 import org.littletonrobotics.junction.Logger;
@@ -11,6 +12,12 @@ import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class Vision extends SubsystemBase {
+
+  private FieldSim m_fieldSim;
+
+  public void registerFieldSim(FieldSim fieldSim) {
+    m_fieldSim = fieldSim;
+  }
 
   PhotonCamera camera = new PhotonCamera("Limelight2");
   PhotonPoseEstimator photonPoseEstimator =
@@ -23,10 +30,6 @@ public class Vision extends SubsystemBase {
   public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
     return photonPoseEstimator.update();
   }
-
-  // private Pose2d estimatedPose = new Pose2d();
-
-  public Vision() {}
 
   public boolean isCameraConnected() {
     return camera.isConnected();
@@ -51,47 +54,12 @@ public class Vision extends SubsystemBase {
     }
   }
 
-  // public PhotonTrackedTarget getTarget() {
-  //   if (camera.isConnected()) {
-  //     var result = camera.getLatestResult();
-  //     List<PhotonTrackedTarget> targets = result.getTargets();
-  //     if (!targets.isEmpty()) {
-  //       return targets.get(0);
-  //     } else {
-  //       return null;
-  //     }
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
-  // public Pose3d getEstimatedFieldPose() {
-  //   var result = camera.getLatestResult();
-
-  //   if (result.hasTargets()) {
-  //     PhotonTrackedTarget target = result.getBestTarget();
-
-  //     if (target != null) {
-  //       var aprilTagDetection = VISION.aprilTagFieldLayout.getTagPose(target.getFiducialId());
-  //       if (aprilTagDetection.isPresent()) {
-  //         return PhotonUtils.estimateFieldToRobotAprilTag(
-  //             target.getBestCameraToTarget(), aprilTagDetection.get(), VISION.robotToCam);
-  //       }
-  //     }
-  //   }
-
-  //   return new Pose3d();
-  // }
-
   private void updateLog() {
     Logger.recordOutput("vision/isCameraConnected", isCameraConnected());
 
     if (isCameraConnected()) {
       Logger.recordOutput("vision/isAprilTagDetected", isAprilTagDetected());
       Logger.recordOutput("vision/getTargets", getTargets());
-      // Logger.recordOutput("vision/estimatedPose", estimatedPose);
-      // Logger.recordOutput("vision/estimated3DPose", getEstimatedFieldPose());
-      // Logger.recordOutput("vision/estimatedGlobalPose", getEstimatedGlobalPose());
     }
   }
 
@@ -104,5 +72,11 @@ public class Vision extends SubsystemBase {
     // This method will be called once per scheduler run
     updateLog();
     smartDashboard();
+    if (m_fieldSim != null) {
+      var pose = getEstimatedGlobalPose();
+      if (pose.isPresent()) {
+        m_fieldSim.updateVisionPose(pose.get().estimatedPose.toPose2d());
+      }
+    }
   }
 }
