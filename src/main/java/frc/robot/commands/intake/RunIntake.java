3df7e378c4bb4f4dda0a635013b336bdfaf3a917
Mason-Beard <Mason-Beard@users.noreply.github.com>
2024-02-11 02:44:15 +0000

    Automatically applied spotless

--- src/main/java/frc/robot/commands/intake/RunIntake.java
+++ src/main/java/frc/robot/commands/intake/RunIntake.java
@@ -23,13 +23,13 @@ public class RunIntake extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intake.setSpeed(m_speed, m_speed2);}
+    m_intake.setSpeed(m_speed, m_speed2);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
