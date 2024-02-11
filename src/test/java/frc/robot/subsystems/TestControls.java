b8bcc0ef9b899e2803935a099bd3d217ca4971f7
jonathandao0 <jonathandao0@users.noreply.github.com>
2024-02-11 02:07:05 +0000

    Automatically applied spotless

--- src/test/java/frc/robot/subsystems/TestControls.java
+++ src/test/java/frc/robot/subsystems/TestControls.java
@@ -1,60 +1,58 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
-import com.pathplanner.lib.path.PathPlannerPath;
-import edu.wpi.first.hal.AllianceStationID;
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-import frc.robot.RobotContainer;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.littletonrobotics.conduit.ConduitApi;
-import org.littletonrobotics.junction.inputs.LoggedDriverStation;
-
 import static frc.robot.utils.TestUtils.refreshAkitData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.hal.AllianceStationID;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+
 public class TestControls {
 
-    Controls m_controls = new Controls();
+  Controls m_controls = new Controls();
 
-    @BeforeEach
-    public void constructDevices() {
-        assert HAL.initialize(500, 0);
+  @BeforeEach
+  public void constructDevices() {
+    assert HAL.initialize(500, 0);
 
-        /* enable the robot */
-        DriverStationSim.setDsAttached(true);
-        DriverStationSim.setEnabled(false);
-        DriverStationSim.notifyNewData();
-        refreshAkitData();
+    /* enable the robot */
+    DriverStationSim.setDsAttached(true);
+    DriverStationSim.setEnabled(false);
+    DriverStationSim.notifyNewData();
+    refreshAkitData();
 
-        /* delay ~100ms so the devices can start up and enable */
-        Timer.delay(0.100);
-    }
+    /* delay ~100ms so the devices can start up and enable */
+    Timer.delay(0.100);
+  }
 
-    @Disabled("Issues with AdvantageKit implementation. Test with Simulation")
-    public void testAutoStartPoseFlip() {
-        DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
-        DriverStationSim.notifyNewData();
-        refreshAkitData();
-        m_controls.periodic();
-        m_controls.updateStartPose("FourPieceNear");
+  @Disabled("Issues with AdvantageKit implementation. Test with Simulation")
+  public void testAutoStartPoseFlip() {
+    DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+    DriverStationSim.notifyNewData();
+    refreshAkitData();
+    m_controls.periodic();
+    m_controls.updateStartPose("FourPieceNear");
 
-        PathPlannerPath path1 = PathPlannerPath.fromPathFile("fourpiecept1");
+    PathPlannerPath path1 = PathPlannerPath.fromPathFile("fourpiecept1");
 
-        assertEquals(path1.getPreviewStartingHolonomicPose().getTranslation(),  m_controls.getStartPose().getTranslation());
+    assertEquals(
+        path1.getPreviewStartingHolonomicPose().getTranslation(),
+        m_controls.getStartPose().getTranslation());
 
-        DriverStationSim.setAllianceStationId(AllianceStationID.Red1);
-        DriverStationSim.notifyNewData();
-        refreshAkitData();
-        m_controls.periodic();
-        m_controls.updateStartPose("FourPieceNear");
+    DriverStationSim.setAllianceStationId(AllianceStationID.Red1);
+    DriverStationSim.notifyNewData();
+    refreshAkitData();
+    m_controls.periodic();
+    m_controls.updateStartPose("FourPieceNear");
 
-        var flippedPath = path1.flipPath();
+    var flippedPath = path1.flipPath();
 
-        assertEquals(flippedPath.getPreviewStartingHolonomicPose().getTranslation(),  m_controls.getStartPose().getTranslation());
-    }
+    assertEquals(
+        flippedPath.getPreviewStartingHolonomicPose().getTranslation(),
+        m_controls.getStartPose().getTranslation());
+  }
 }
