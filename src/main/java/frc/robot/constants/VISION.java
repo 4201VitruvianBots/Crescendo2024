f02cef35435b696b56b631c91c7ce5e86443c955
Benjamin Chan <benjamin.v.chan@gmail.com>
2024-02-14 10:22:08 -0800

    Updated vision constants
    Still need the exact camera dimensions, distances, and angles from center of robot in order for pose estimation to fully work

--- src/main/java/frc/robot/constants/VISION.java
+++ src/main/java/frc/robot/constants/VISION.java
@@ -12,14 +12,23 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public final class VISION {
-  public static final double limelightHeight = Units.inchesToMeters(35);
+  public static final double aprilTagLimelightCamerasHeight = Units.inchesToMeters(35);
+  public static final double aprilTagLimelightCamerasYaw = 54; // Just the angle offset from center of robot bar to center of camera
+  public static final double aprilTagLimelightCamerasPitch = 0;
+    public static final double aprilTagLimelightCamerasRoll = 0;
+  public static final double aprilTagLimelightCamerasXDistanceFromCenter = Units.inchesToMeters(35); // TODO
+  public static final double aprilTagLimelightCamerasYDistanceFromCenter = Units.inchesToMeters(35); // TODO
 
   public static final AprilTagFieldLayout aprilTagFieldLayout =
       AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
-  // Camera offset from robot center in meters. Will need to update this
-  public static final Transform3d robotToCam =
-      new Transform3d(new Translation3d(0.5, 0.0, limelightHeight), new Rotation3d(0, 0, 0));
+  // Camera offset from robot center in meters. Will need to update this based on which camera is on right and which is on left (add negative values)
+  public static final Transform3d robotToAprilTagLimelightCameraA =
+      new Transform3d(new Translation3d(aprilTagLimelightCamerasXDistanceFromCenter, aprilTagLimelightCamerasYDistanceFromCenter, aprilTagLimelightCamerasHeight), new Rotation3d(0, 0, 0));
+
+  // Camera offset from robot center in meters. Will need to update this based on which camera is on right and which is on left (add negative values)
+  public static final Transform3d robotToAprilTagLimelightCameraB =
+      new Transform3d(new Translation3d(aprilTagLimelightCamerasXDistanceFromCenter, aprilTagLimelightCamerasYDistanceFromCenter, aprilTagLimelightCamerasHeight), new Rotation3d(0, 0, 0));
 
   public enum CAMERA_TYPE {
     LIMELIGHT,
