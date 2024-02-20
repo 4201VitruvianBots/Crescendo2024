8da96c54566c58d709c7727a1753599b5b19d475
Jonathan Dao <jonathandao0@gmail.com>
2024-02-20 00:32:49 -0800

    Final pull changes

--- src/main/java/frc/robot/commands/led/GetSubsystemStates.java
+++ src/main/java/frc/robot/commands/led/GetSubsystemStates.java
@@ -1,81 +1,81 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands.led;
+package frc.robot.commands.led;
 
-// import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.constants.LED;
-// import frc.robot.subsystems.AmpShooter;
-// import frc.robot.subsystems.Climber;
-// import frc.robot.subsystems.Intake;
-// import frc.robot.subsystems.LEDSubsystem;
-// import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.LED;
+import frc.robot.subsystems.AmpShooter;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.Shooter;
 
-// public class GetSubsystemStates extends Command {
+public class GetSubsystemStates extends Command {
 
-//   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-//   private final LEDSubsystem m_led;
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final LEDSubsystem m_led;
 
-//   private final AmpShooter m_ampShooter;
-//   private final Climber m_climber;
-//   private final Intake m_intake;
-//   private final Shooter m_shooter;
-//   private boolean isIntaking;
-//   private boolean isClimbing;
-//   private boolean isScoringSpeaker;
-//   private boolean isUptaking;
-//   private boolean isDisabled;
-//   private boolean isEnabled;
+  private final AmpShooter m_ampShooter;
+  private final Climber m_climber;
+  private final Intake m_intake;
+  private final Shooter m_shooter;
+  private boolean isIntaking;
+  private boolean isClimbing;
+  private boolean isScoringSpeaker;
+  private boolean isUptaking;
+  private boolean isDisabled;
+  private boolean isEnabled;
 
-//   /** Sets the LED based on the subsystems' statuses */
-//   public GetSubsystemStates(
-//       LEDSubsystem led, Intake intake, Climber climber, AmpShooter ampShooter, Shooter shooter) {
-//     m_led = led;
-//     m_intake = intake;
-//     m_climber = climber;
-//     m_ampShooter = ampShooter;
-//     m_shooter = shooter;
+  /** Sets the LED based on the subsystems' statuses */
+  public GetSubsystemStates(
+      LEDSubsystem led, Intake intake, Climber climber, AmpShooter ampShooter, Shooter shooter) {
+    m_led = led;
+    m_intake = intake;
+    m_climber = climber;
+    m_ampShooter = ampShooter;
+    m_shooter = shooter;
 
-//     addRequirements(m_led);
-//   }
+    addRequirements(m_led);
+  }
 
-//   // Called when the command is initially scheduled.
-//   @Override
-//   public void initialize() {}
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
 
-//   // Called every time the scheduler runs while the command is scheduled.
-//   @Override
-//   public void execute() {
-//     // isIntaking = m_intake.getIntakeState() == INTAKE_STATE.INTAKING;
-//     // isUptaking = m_uptake.getUptakeState() == UPTAKE_STATE.UPTAKING;
-//     isDisabled = DriverStation.isDisabled();
-//     isEnabled = !isDisabled;
-//     // isClimbing = m_climber.getClimberState() == SUBSYSTEM_STATES.CLIMBING;
-//     // isScoringSpeaker = m_shooter.getShooterState() == SUBSYSTEM_STATES.SCORE_SPEAKER;
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // isIntaking = m_intake.getIntakeState() == INTAKE_STATE.INTAKING;
+    // isUptaking = m_uptake.getUptakeState() == UPTAKE_STATE.UPTAKING;
+    isDisabled = DriverStation.isDisabled();
+    isEnabled = !isDisabled;
+    // isClimbing = m_climber.getClimberState() == SUBSYSTEM_STATES.CLIMBING;
+    // isScoringSpeaker = m_shooter.getShooterState() == SUBSYSTEM_STATES.SCORE_SPEAKER;
 
-//     // the prioritized state to be expressed to the LEDs
-//     // set in order of priority to be expressed from the least priority to the
-//     // highest priority
-//     if (isDisabled) {
-//       m_led.expressState(LED.SUBSYSTEM_STATES.DISABLED);
-//     } else if (isIntaking) {
-//       m_led.expressState(LED.SUBSYSTEM_STATES.INTAKING);
-//     } else if (isUptaking) {
-//       m_led.expressState(LED.SUBSYSTEM_STATES.UPTAKING);
-//     } else if (isEnabled) {
-//       m_led.expressState(LED.SUBSYSTEM_STATES.ENABLED);
-//     }
-//   }
+    // the prioritized state to be expressed to the LEDs
+    // set in order of priority to be expressed from the least priority to the
+    // highest priority
+    if (isDisabled) {
+      m_led.expressState(LED.SUBSYSTEM_STATES.DISABLED);
+    } else if (isIntaking) {
+      m_led.expressState(LED.SUBSYSTEM_STATES.INTAKING);
+    } else if (isUptaking) {
+      m_led.expressState(LED.SUBSYSTEM_STATES.UPTAKING);
+    } else if (isEnabled) {
+      m_led.expressState(LED.SUBSYSTEM_STATES.ENABLED);
+    }
+  }
 
-//   // Called once the command ends or is interrupted.
-//   @Override
-//   public void end(boolean interrupted) {}
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
-//   // Returns true when the command should end.
-//   @Override
-//   public boolean isFinished() {
-//     return false;
-//   }
-// }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/constants/SHOOTER.java
+++ src/main/java/frc/robot/constants/SHOOTER.java
@@ -26,7 +26,7 @@ public final class SHOOTER {
   public static final double gearRatioBottom = 20.0 / 28.0;
   public static final DCMotor ShooterBottomGearbox = DCMotor.getFalcon500(1);
 
-  public static final DCMotor ShooterTopGearbox = DCMotor.getKrakenX60Foc(1);
+  public static final DCMotor ShooterTopGearbox = DCMotor.getKrakenX60(1);
   public static final double gearRatioTop = 20.0 / 28.0;
 
   public static final double Inertia = 0.001;
