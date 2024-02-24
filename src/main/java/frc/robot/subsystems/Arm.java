be039d23cc0497b9d500b842273d139bfb847fbc
SherazF <SherazF@users.noreply.github.com>
2024-02-24 20:32:27 +0000

    Automatically applied spotless

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -88,7 +88,7 @@ public class Arm extends SubsystemBase {
 
     // Simulation setup
     SmartDashboard.putData(this);
-    
+
     // m_armMotor.setPosition(Units.degreesToRotations(ARM.startingAngleDegrees));
   }
 
@@ -120,7 +120,7 @@ public class Arm extends SubsystemBase {
   }
 
   public double getCurrentAngle() {
-    return Units.rotationsToDegrees(getCurrentRotation() * (1.0/140.0));
+    return Units.rotationsToDegrees(getCurrentRotation() * (1.0 / 140.0));
   }
 
   public void setControlMode(ROBOT.CONTROL_MODE mode) {
