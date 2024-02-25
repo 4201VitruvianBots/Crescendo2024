43d88ea064d2b85ca7b404f5eca288414ddceec7
Gavin P <gavinjpeeler@gmail.com>
2024-02-25 14:14:19 -0800

    Fixed toggle command

--- src/main/java/frc/robot/commands/climber/ToggleClimberControlMode.java
+++ src/main/java/frc/robot/commands/climber/ToggleClimberControlMode.java
@@ -5,17 +5,17 @@
 package frc.robot.commands.climber;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.constants.ROBOT;
 import frc.robot.constants.ROBOT.CONTROL_MODE;
 import frc.robot.subsystems.Climber;
 
 public class ToggleClimberControlMode extends InstantCommand {
   private final Climber m_climber;
-  private final CONTROL_MODE m_mode;
 
   /** Creates a new ToggleClimberControlMode. */
-  public ToggleClimberControlMode(Climber climber, CONTROL_MODE mode) {
+  public ToggleClimberControlMode(Climber climber) {
     m_climber = climber;
-    m_mode = mode;
+    
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_climber);
   }
@@ -23,7 +23,9 @@ public class ToggleClimberControlMode extends InstantCommand {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    switch(m_mode) {
+    ROBOT.CONTROL_MODE climberControlMode = m_climber.getClosedLoopControlMode();
+    
+    switch(climberControlMode) {
       case OPEN_LOOP:
           m_climber.setClosedLoopControlMode(CONTROL_MODE.CLOSED_LOOP);          
           m_climber.resetTrapezoidState();
