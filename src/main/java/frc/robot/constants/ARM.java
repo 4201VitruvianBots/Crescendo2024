03f38859c6b9d6c875b457eda3c0d44f74e905b5
Mason Beard <Masonbeard06@gmail.com>
2024-02-25 19:35:50 -0800

    Amp Changes

--- src/main/java/frc/robot/constants/ARM.java
+++ src/main/java/frc/robot/constants/ARM.java
@@ -35,7 +35,7 @@ public final class ARM {
   public static final double kMaxArmAcceleration = 10;
 
   public enum ARM_SETPOINT {
-    STOWED(Units.degreesToRotations(-35.0)),
+    STOWED(Units.degreesToRotations(-40.0)),
     STAGED(Units.degreesToRotations(75.0)),
     FORWARD(Units.degreesToRotations(120.0));
 
