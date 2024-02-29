797d01fa7c1322418332bcc61c388c7a4f40d911
Jonathan Dao <jonathandao0@gmail.com>
2024-02-28 19:51:13 -0800

    Removed unused libraries

--- src/main/java/frc/robot/utils/TrajectoryUtils.java
+++ src/main/java/frc/robot/utils/TrajectoryUtils.java
@@ -4,15 +4,11 @@
 
 package frc.robot.utils;
 
-import com.choreo.lib.Choreo;
-import com.choreo.lib.ChoreoTrajectory;
 import com.pathplanner.lib.commands.FollowPathHolonomic;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.SWERVE;
 import frc.robot.constants.SWERVE.DRIVE;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -61,26 +57,26 @@ public class TrajectoryUtils {
         swerveDrive);
   }
 
-  public static Command generateChoreoCommand(
-      CommandSwerveDrivetrain swerveDrive, String pathName, double maxSpeed, boolean flipPath) {
-    ChoreoTrajectory traj = Choreo.getTrajectory(pathName);
-
-    return generateChoreoCommand(swerveDrive, traj, maxSpeed, flipPath);
-  }
-
-  public static Command generateChoreoCommand(
-      CommandSwerveDrivetrain swerveDrive,
-      ChoreoTrajectory traj,
-      double maxSpeed,
-      boolean flipPath) {
-    return Choreo.choreoSwerveCommand(
-        traj,
-        () -> swerveDrive.getState().Pose,
-        new PIDController(DRIVE.kP_X, DRIVE.kI_X, DRIVE.kD_X),
-        new PIDController(DRIVE.kP_X, DRIVE.kI_X, DRIVE.kD_X),
-        new PIDController(DRIVE.kP_Theta, DRIVE.kI_Theta, DRIVE.kD_Theta),
-        swerveDrive::setChassisSpeedControlNormal,
-        () -> flipPath,
-        swerveDrive);
-  }
+  //  public static Command generateChoreoCommand(
+  //      CommandSwerveDrivetrain swerveDrive, String pathName, double maxSpeed, boolean flipPath) {
+  //    ChoreoTrajectory traj = Choreo.getTrajectory(pathName);
+  //
+  //    return generateChoreoCommand(swerveDrive, traj, maxSpeed, flipPath);
+  //  }
+  //
+  //  public static Command generateChoreoCommand(
+  //      CommandSwerveDrivetrain swerveDrive,
+  //      ChoreoTrajectory traj,
+  //      double maxSpeed,
+  //      boolean flipPath) {
+  //    return Choreo.choreoSwerveCommand(
+  //        traj,
+  //        () -> swerveDrive.getState().Pose,
+  //        new PIDController(DRIVE.kP_X, DRIVE.kI_X, DRIVE.kD_X),
+  //        new PIDController(DRIVE.kP_X, DRIVE.kI_X, DRIVE.kD_X),
+  //        new PIDController(DRIVE.kP_Theta, DRIVE.kI_Theta, DRIVE.kD_Theta),
+  //        swerveDrive::setChassisSpeedControlNormal,
+  //        () -> flipPath,
+  //        swerveDrive);
+  //  }
 }
